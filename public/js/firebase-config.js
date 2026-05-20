7343e2ac5076c5d61996849859c7c3f934be8b87
celsogiodias <celsogiodias@gmail.com>
2026-05-20 09:20:58 -0300

    Add Firebase configuration and initialize services

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,1 +1,23 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdQn-SeakKc8cLVATPjCW8x1RzvtjnIBs",
+  authDomain: "clinica-ativamente.firebaseapp.com",
+  projectId: "clinica-ativamente",
+  storageBucket: "clinica-ativamente.firebasestorage.app",
+  messagingSenderId: "73202377832",
+  appId: "1:73202377832:web:5d42477d7feea9d8b587dd"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+db.enablePersistence()
+  .catch((err) => {
+    if (err.code === 'failed-precondition') {
+      console.log('Persistência falhou: várias abas abertas');
+    } else if (err.code === 'unimplemented') {
+      console.log('Persistência não disponível neste navegador');
+    }
+  });
